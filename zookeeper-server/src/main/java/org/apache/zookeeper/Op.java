6c8ec7f3ebbf1383bda3942b6f5f417ed0cc5f38
xhs <xuhuasong@eastmoney.com>
2024-05-10 18:10:22 +0800

    zookeeper开发环境启动

--- zookeeper-server/src/main/java/org/apache/zookeeper/Op.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/Op.java
@@ -40,7 +40,7 @@ import org.apache.zookeeper.server.EphemeralType;
  * Sub-classes of Op each represent each detailed type but should not normally be referenced except via
  * the provided factory methods.
  *
- * @中文
+ * @chinese
  *  表示多操作事务中的单个操作。
  *  每个操作可以是创建、更新、删除、版本检查，也可以是读取操作，
  *  如getChildren或getData。Op的每个子类代表每个详细的类型，但通常不应被引用，除非通过提供的工厂方法。
